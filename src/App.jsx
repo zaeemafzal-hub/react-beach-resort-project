748671b68e8f67b7ad57241ddd7e346a9ce26527
zaeemafzal <zaeeemafzal@gmail.com>
2025-12-28 18:46:48 +0500

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <Router>
       
         <Navbar />
         <Routes>
@@ -18,7 +18,7 @@ function App() {
           <Route path="*" element={<Error />} />
         </Routes>
       
-    </>
+    </Router>
   );
 }
 
